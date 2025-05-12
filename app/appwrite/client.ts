09c99aaf7db7fd747117f46a9a46d7360b54bf39
Vaishnotiwari12 <vaishnotiwari12@gmail.com>
2025-05-13 02:30:01 +0530

    travelling-dashboard

---
+++ app/appwrite/client.ts
@@ -0,0 +1,32 @@
+import { Client, Account, Databases, Storage } from 'appwrite';
+
+export const appwriteConfig = {
+    endpointUrl: import.meta.env.VITE_APPWRITE_API_ENDPOINT,
+    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    apiKey: import.meta.env.VITE_APPWRITE_API_KEY,
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    userCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
+    tripCollectionId: import.meta.env.VITE_APPWRITE_TRIPS_COLLECTION_ID,
+    // itineraryCollectionId: import.meta.env.VITE_APPWRITE_ITINERARY_COLLECTION_ID
+};
+
+// Create client instances
+const client = new Client();
+const serverClient = new Client();
+
+// Configure client for browser
+client
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
+// Configure client for server
+serverClient
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    // .setKey(appwriteConfig.apiKey);
+
+// Initialize services
+const account = new Account(client);
+const database = new Databases(serverClient);
+const storage = new Storage(serverClient);
+
+export { client, account, database, storage };
