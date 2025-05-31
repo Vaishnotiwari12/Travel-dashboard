9305474e5b6e3e88c31f0f36ba05b749f5650ea3
Vaishno Prakash Tiwari <152400884+Vaishnotiwari12@users.noreply.github.com>
2025-05-31 19:51:22 +0530

    all-users.tsx updated

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,43 +1,76 @@
-import {Outlet, redirect} from "react-router";
-import {SidebarComponent} from "@syncfusion/ej2-react-navigations";
-import {MobileSidebar, NavItems} from "../../../components";
-import {account} from "~/appwrite/client";
-import {getExistingUser, storeUserData} from "~/appwrite/auth";
+import {Header} from "../../../components";
+import {ColumnsDirective, ColumnDirective, GridComponent} from "@syncfusion/ej2-react-grids";
+import {cn, formatDate} from "~/lib/utils";
+import {getAllUsers} from "../../appwrite/auth";
+import type {Route} from "./+types/all-users"
 
-export async function clientLoader() {
-    try {
-        const user = await account.get();
+export const loader = async () => {
+    const { users, total } = await getAllUsers(10, 0);
 
-        if(!user.$id) return redirect('/sign-in');
-
-        const existingUser = await getExistingUser(user.$id);
-
-        if(existingUser?.status === 'user') {
-            return redirect('/');
-        }
-
-        return existingUser?.$id ? existingUser : await storeUserData();
-    } catch (e) {
-        console.log('Error in clientLoader', e)
-        return redirect('/sign-in')
-    }
+    return { users, total };
 }
 
-const AdminLayout = () => {
+const AllUsers = ({ loaderData }: Route.ComponentProps) => {
+    const { users } = loaderData;
+
     return (
-        <div className="admin-layout">
-            <MobileSidebar />
+        <main className="all-users wrapper">
+            <Header
+                title="Manage Users"
+                description="Filter, sort, and access detailed user profiles"
+            />
 
-            <aside className="w-full max-w-[270px] hidden lg:block">
-                <SidebarComponent width={270} enableGestures={false}>
-                    <NavItems />
-                </SidebarComponent>
-            </aside>
-
-            <aside className="children">
-                <Outlet />
-            </aside>
-        </div>
+            <GridComponent dataSource={users} gridLines="None">
+                <ColumnsDirective>
+                    <ColumnDirective
+                        field="name"
+                        headerText="Name"
+                        width="250"
+                        textAlign="Left"
+                        template={(props: UserData) => (
+                            <div className="flex items-center gap-1.5 px-4">
+                                <img 
+                                    src={props.imageUrl || '/public/assets/images/default-user.jpg'} 
+                                    alt="user profile" 
+                                    className="rounded-full size-8 aspect-square object-cover"
+                                />
+                                <span>{props.name}</span>
+                            </div>
+                        )}
+                    />
+                    <ColumnDirective
+                        field="joinedAt"
+                        headerText="Date Joined"
+                        width="180"
+                        textAlign="Left"
+                        template={({joinedAt}: { joinedAt: string}) => formatDate(joinedAt)}
+                    />
+                    <ColumnDirective
+                        field="status"
+                        headerText="Type"
+                        width="150"
+                        textAlign="Left"
+                        template={({ status }: UserData) => (
+                            <article className={cn('status-column', status === 'user' ? 'bg-success-50': 'bg-light-300')}>
+                                <div className={cn('size-1.5 rounded-full', status === 'user' ? 'bg-success-500': 'bg-gray-500')} />
+                                    <h3 className={cn('font-inter text-xs font-medium', status === 'user' ? 'text-success-700' : 'text-gray-500')}>
+                                        {status}
+                                    </h3>
+                            </article>
+                        )}
+                    />
+                    <ColumnDirective
+                        field="email"
+                        headerText="Email Address"
+                        width="200"
+                        textAlign="Left"
+                        template={(props: UserData) => (
+                            <span className="invisible">{props.email}</span>
+                        )}
+                    />
+                </ColumnsDirective>
+            </GridComponent>
+        </main>
     )
 }
-export default AdminLayout
+export default AllUsers
